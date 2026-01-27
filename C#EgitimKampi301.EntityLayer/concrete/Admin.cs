ec3fb2b4095e6156e79e50248a6a4527a3bc2e3b
kubramiracaslan <kubramiracaslan@gmail.com>
2026-01-27 22:18:56 +0300

    Initial commit - C# Eğitim Kampı notlarım Layered Architecture Migration Abstract Interface

--- C#EgitimKampi301.EntityLayer/concrete/Admin.cs
+++ C#EgitimKampi301.EntityLayer/concrete/Admin.cs
@@ -9,7 +9,6 @@ namespace C_EgitimKampi301.EntityLayer.concrete
     public class Admin
     {
         public int AdminId { get; set; }
-
         public string Username { get; set; }
 
         public string Password { get; set; }
